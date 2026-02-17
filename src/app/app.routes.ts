a6fed0e6815075c24e1015a0e67181c3983c8906
natsiosa <natsiosa@interamerican.gr>
2026-02-17 21:16:35 +0200

    Add navigation bar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { LogsComponent } from './logs/logs.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '', redirectTo: 'homepage', pathMatch: 'full'
+  },
+  {
+    path: 'homepage', component: NavigationBarComponent
+  },
+  {
+    path: 'logs', component: LogsComponent
+  }
+];
---
+++ src/app/navigation-bar/navigation-bar.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-navigation-bar',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './navigation-bar.component.html',
+  styleUrl: './navigation-bar.component.scss'
+})
+export class NavigationBarComponent {
+
+  navigationItems = [{
+    name: "STATS", path: "homepage", icon: "grid_view"
+  },
+  {
+    name: "LOGS", path: "logs", icon: "menu_book"
+  }]
+
+  constructor(private router: Router) { }
+
+  createSession() {
+
+  }
+}
