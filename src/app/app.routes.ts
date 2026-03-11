432f8c72852c6781af14783258d6f42510f29874
natsiosa <natsiosa@interamerican.gr>
2026-03-11 11:24:48 +0200

    Update structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
-import { LogsComponent } from './logs/logs.component';
-import { HomepageComponent } from './homepage/homepage.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { LogsComponent } from './components/logs/logs.component';
 
 export const routes: Routes = [
   {
